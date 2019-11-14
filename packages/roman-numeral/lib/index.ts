10a99af74782dcc101bb8b5875e8ad515089226e
László Szűcs <laszlo.szucs@klarna.com>
2019-11-14 22:03:17 +0100

    Implement 4

--- packages/roman-numeral/lib/index.ts
+++ packages/roman-numeral/lib/index.ts
@@ -1,5 +1,21 @@
 export const convertArabicToRomanNumeral = (arabic: number): string => {
   // if found more than 3 of the same character in sequence, then 
   // only take the first of them and append a V
-  return 'I'.repeat(arabic)
+
+  let roman = 'I'.repeat(arabic)
+
+  if (foundMoreThanThreeOf(roman, 'I')) {
+    // get the last char of $roman => 'I'
+    // append 'V'
+    
+    const lastChar = Array.from(roman).pop() // => I
+    roman = lastChar + 'V'
+  }
+  
+  return roman
+}
+
+function foundMoreThanThreeOf(string: string, char: string) {
+  const matcher = new RegExp(`${char}${char}${char}`)
+  return matcher.test(string)
 }
