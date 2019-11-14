9c660179fd350159f4c31a0e8a4a5c7bd9956a3d
László Szűcs <laszlo.szucs@klarna.com>
2019-11-14 21:47:13 +0100

    Discussion around 4

--- packages/roman-numeral/lib/index.spec.ts
+++ packages/roman-numeral/lib/index.spec.ts
@@ -1,9 +1,17 @@
-import { convertArabicToRomanNumeral } from './index'
+import { convertArabicToRomanNumeral } from '.'
 
 describe('Roman numeral converter', () => {
   describe('convertArabicToRomanNumeral', () => {
     it('should convert 1', () => {
       expect(convertArabicToRomanNumeral(1)).toBe('I')
+    })
+
+    it('should convert 2', () => {
+      expect(convertArabicToRomanNumeral(2)).toBe('II')
+    })
+
+    it('should convert 4', () => {
+      expect(convertArabicToRomanNumeral(4)).toBe('IV')
     })
   })
 })
--- packages/roman-numeral/lib/index.ts
+++ packages/roman-numeral/lib/index.ts
@@ -1,3 +1,5 @@
 export const convertArabicToRomanNumeral = (arabic: number): string => {
-  return 'I'
+  // if found more than 3 of the same character in sequence, then 
+  // only take the first of them and append a V
+  return 'I'.repeat(arabic)
 }
