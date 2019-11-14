4fff2c808fbf97862c4cb36622c8be6fcc32f23d
László Szűcs <laszlo.szucs@klarna.com>
2019-11-14 20:16:21 +0100

    Initial commit

---
+++ packages/roman-numeral/lib/index.spec.ts
@@ -0,0 +1,7 @@
+describe('Roman numeral converter', () => {
+  describe('convertArabicToRomanNumeral', () => {
+    it('should work', () => {
+      throw new Error('NOT IMPLEMENTED')
+    })
+  })
+})
